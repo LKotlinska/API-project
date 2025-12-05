6bf7b5fd867d0af187471eea185db7af70537281
Your Name <laura.kotlinska.96@gmail.com>
2025-12-05 15:17:33 +0100

    Making the data useble inside main.js and fixing a bug with double request

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // QUESTION: WILL THIS COUNT AS HAVING MULTIPLE APIs?
-export function getPairedRecipes(wineType) {
+export async function getPairedRecipes(wineType) {
   const foodAPIBaseUrl = `https://api.spoonacular.com/recipes/`;
   const wineAPIBaseUrl = "https://api.spoonacular.com/food/wine/";
 
@@ -12,30 +12,30 @@ export function getPairedRecipes(wineType) {
     // Get dish examples from wine API
     const response = await fetch(wineAPIBaseUrl + "dishes?" + pairingSearch);
     const pairingData = await response.json();
-    // Create array with pairings
-    const pairings = pairingData["pairings"];
-
-    const getRecipe = async (query) => {
-      const dishSearch = new URLSearchParams({
-        query: query,
-        number: 1,
-        apiKey: "bdf9eb43726a4fec87cc0d370296753d",
-      });
-      
-      const response = await fetch(foodAPIBaseUrl + "complexSearch?" + dishSearch);
-      const dishData = await response.json();
-      return dishData;
-    };
-    getRecipe();
-
-    // Loops through pairings array to get a recipe per ingredient
-    const getDishesByIngredient = async () => {
-      for (const query of pairings) {
-        await getRecipe(query);
-      }
-    };
-    getDishesByIngredient();
+    console.log(pairingData);
+    return pairingData["pairings"];
   };
-  getPairings();
+  const getRecipe = async (query) => {
+    const dishSearch = new URLSearchParams({
+      query,
+      number: 1,
+      apiKey: "bdf9eb43726a4fec87cc0d370296753d",
+    });
 
+    const response = await fetch(
+      foodAPIBaseUrl + "complexSearch?" + dishSearch
+    );
+    const dishData = await response.json();
+    return dishData;
+  };
+
+  const pairings = await getPairings();
+  const recipes = [];
+  for (const query of pairings) {
+    // Set timeout cuz stoopid max 5 request/s
+    setTimeout(async () => {
+      recipes.push((await getRecipe(query)).results[0]);
+    }, 250);
+  }
+  return recipes;
 }
