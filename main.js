81232114f4be9ac0dfb160e16c9f1941c3587ea1
Your Name <laura.kotlinska.96@gmail.com>
2025-12-08 16:46:20 +0100

    Show selected wine info and recipe examples

--- main.js
+++ main.js
@@ -2,18 +2,55 @@ import { getPairedRecipes } from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const dropdown = document.getElementById("wine-select");
-  const button = document.getElementById("submit-winetype");
+  const form = document.getElementById("submit-winetype");
 
-  button.addEventListener("click", () => {
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
     // On click: select and submit value
     const dropdown = document.getElementById("wine-select");
     const wineType = dropdown.value;
-    // Reset to default
-    dropdown.value = "default";
-    
-    getPairedRecipes(wineType);
+
+    const { wineInfo, recipes } = await getPairedRecipes(wineType);
+
+    // ---- Show info about selected wine
+    function showSelectedWine(wineType, wineInfo) {
+      const wineSelected = document.querySelector(".wine-selected");
+      // Clear previous content
+      wineSelected.innerHTML = "";
+      wineType = formatName(wineType);
+      const wineName = document.createElement("p");
+      wineName.innerText = wineType + ", great choice!";
+      wineSelected.appendChild(wineName);
+
+      const wineDesc = document.createElement("p");
+      wineDesc.innerText = wineInfo;
+      wineSelected.appendChild(wineDesc);
+    }
+    showSelectedWine(wineType, wineInfo);
+
+    function showRecipes() {
+      const recipeSection = document.getElementById("recipe-section");
+      // Clear previous content
+      recipeSection.innerHTML = "";
+
+      recipes.forEach((recipe) => {
+        // Create details toggle
+        const details = document.createElement('details');
+        recipeSection.appendChild(details);
+        const summary = document.createElement('summary');
+        details.appendChild(summary);
+        const title = document.createElement("h4");
+        title.innerText = recipe.title;
+        summary.appendChild(title);
+      });
+      
+      
+
+    }
+    showRecipes();
+    dropdown.value = null;
   });
-  
+
   // Fetch and display wine selection
   try {
     fetch("resources/wines.json")
@@ -21,13 +58,13 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((wines) => {
         for (let category in wines) {
           // Name cleanup and create selection groups
-          const categoryName = replaceUnderscore(category);
+          const categoryName = formatName(category);
           const optgroup = document.createElement("optgroup");
           optgroup.setAttribute("label", categoryName);
           dropdown.appendChild(optgroup);
 
           wines[category].forEach((wine) => {
-            const wineName = replaceUnderscore(wine);
+            const wineName = formatName(wine);
             const option = document.createElement("option");
             option.innerText = wineName;
             option.setAttribute("value", wine);
@@ -39,10 +76,10 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(error);
   }
 
-
-
-
-  function replaceUnderscore(string) {
-    return string.replace("_", " ");
+  function formatName(string) {
+  return string
+    .split("_")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
   }
 });
